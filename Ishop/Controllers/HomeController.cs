d4ff92cc7131a8c19f508d7df0712a674fdc9ece
murucharls <murucharls@gmail.com>
2024-02-16 15:20:12 +0300

    Error custom view

--- Ishop/Controllers/HomeController.cs
+++ Ishop/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using Ishop.Infa;
+using Microsoft.ReportingServices.ReportProcessing.ReportObjectModel;
 using System;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -40,6 +43,36 @@ namespace Ishop.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+
+        private string connectionString = ConfigurationManager.ConnectionStrings["Planning"].ConnectionString;
+        public void Glitch(string Message, string User)
+        {
+            Employee_Context emp = new Employee_Context();
+            var ee = emp.employees.Where(c => c.Username == User).FirstOrDefault();
+
+            string messagel = "I wanted to bring to your attention that I recently encountered a system glitch or error while using the platform."
+            + "\n" + "The issue occurred on the following system url : " + Message
+            + "\n" + "thank you ";
+            string query = "INSERT INTO OutgoingEmails (Recipient,Subject,Body,Status,CreatedOn,Trials,Response) VALUES " +
+            "                                          (@Recipient,@Subject, @Body,0,@CreatedOn,@Trials,@Response)";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    var rr = "info@speculate.co.ke,"+ee.Email;
+                    var cc = "System Glich";
+                    command.Parameters.AddWithValue("@Recipient", rr);
+                    command.Parameters.AddWithValue("@Subject", cc);
+                    command.Parameters.AddWithValue("@Body", messagel);
+                    command.Parameters.AddWithValue("@CreatedOn", DateTime.Now);
+                    command.Parameters.AddWithValue("@Trials", 0);
+                    command.Parameters.AddWithValue("@Response", "--waiting--");
+                    command.ExecuteNonQuery();
+                }
+            }
         }
     }
 }
