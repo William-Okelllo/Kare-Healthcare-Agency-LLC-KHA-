64e8191d77a336c3a8cbcbb8539c07c0d6c9bf2c
murucharls <murucharls@gmail.com>
2024-03-06 02:19:49 +0300

    fix

--- Ishop/Controllers/APIController.cs
+++ Ishop/Controllers/APIController.cs
@@ -18,7 +18,7 @@ namespace Ishop.Controllers
 
         public void Updatetimesheetservice()
         {
-            var leavewithbalances = db.leave.Where(c => c.Total_Hours > 0).ToList();
+            var leavewithbalances = db.leave.Where(c => c.Total_Hours > 0 && c.Status ==2).ToList();
             foreach (var leavesb in leavewithbalances)
             {
                 IDays_leave_context dv = new IDays_leave_context();
@@ -58,6 +58,65 @@ namespace Ishop.Controllers
                 }
             }
         }
+
+        public void Fixleaves()
+        {
+            var leavewithbalances = db.leave.Where(c => c.Total_Hours > 0 && c.Status == 2).ToList();
+            foreach (var leavesb in leavewithbalances)
+            {
+                IDays_leave_context dv = new IDays_leave_context();
+                var Dayss = dv.days_Leaves.Where(l => l.Leave_Id == leavesb.Id).FirstOrDefault();
+
+
+                // Adjust GetDatesInRange to exclude weekends
+                List<DateTime> datesInRange = GetDatesInRangeWithoutWeekends(leavesb.From_Date, leavesb.To_Date);
+                
+                Random random = new Random();
+                int HoursOnleave;
+
+                if (Dayss.Time == 1) { HoursOnleave = 8; } else { HoursOnleave = 4; }
+
+                Timesheet_Context TT = new Timesheet_Context();
+                foreach (var date in datesInRange)
+                {
+                    var Sheet = TT.timesheets.Where(i => i.Owner == leavesb.Employee && date >= i.From_Date && date <= i.End_Date).FirstOrDefault();
+                    if (Sheet != null)
+                    {
+                        decimal addedHours = Sheet.Leave + HoursOnleave;
+
+                        // Deduct added hours from leavee.Total_Hours
+
+
+                        Sheet.Leave = addedHours;
+                        Sheet.Tt = Sheet.Direct_Hours + Sheet.InDirect_Hours + Sheet.Leave;
+                        TT.SaveChanges();
+
+                        var leavee = db.leave.Where(c => c.Id == leavesb.Id).FirstOrDefault();
+                        if (leavee != null)
+                        {
+                            leavee.Total_Hours = leavee.Total_Hours - HoursOnleave;
+                            db.SaveChanges(); // Save changes for each iteration
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         // New method to get dates in range excluding weekends
         private List<DateTime> GetDatesInRangeWithoutWeekends(DateTime startDate, DateTime endDate)
